bad2f24ab9ae6d47a60afda3d38714e6bddd6d1e
Anyang Anyang <anyangateny@Anyangs-MacBook-Air.local>
2023-09-07 06:25:47 +0930

    added comments for amin.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from webScraper import scrapeLibrary
 
 import sys
 
+# a way to test the function of scrapeLibrary in CLI
+
 print("Please enter book you would like to search: ")
 bookName = input()
 
